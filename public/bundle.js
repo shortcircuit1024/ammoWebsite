06b8ecae1acea57670f2d6dffe1f7d3e2fa66f39
shortcircuit1024 <shortcircuit1024@gmail.com>
2016-12-20 23:24:41 -0600

    Home and navbar updates

--- public/bundle.js
+++ public/bundle.js
@@ -24882,6 +24882,51 @@
 	    Link = _require.Link,
 	    IndexLink = _require.IndexLink;
 
+	// var Nav = () => {
+	//     return (
+	//         <nav className="navbar navbar-full navbar-dark bg-inverse" style={{
+	//             fontFamily: 'Electrolize'
+	//         }}>
+	//             <img src="/assets/favicon_v3.png" width="30" height="30" alt=""/>
+	//             <span>      </span>
+	//             <IndexLink to="/" className="nav-item" style={{
+	//                 fontSize: '115%',
+	//                 color: '#FFFFFF'
+	//             }} activeClassName="nav-item active" activeStyle={{
+	//                 color: '#F59B45'
+	//             }}>Home</IndexLink>
+	//             <span>      </span>
+	//             <IndexLink to="/scheduler" className="nav-item" style={{
+	//                 fontSize: '115%',
+	//                 color: '#FFFFFF'
+	//             }} activeClassName="nav-item active" activeStyle={{
+	//                 color: '#F59B45'
+	//             }}>Scheduler</IndexLink>
+	//             <span>      </span>
+	//             <IndexLink to="/floatila" className="nav-item" style={{
+	//                 fontSize: '115%',
+	//                 color: '#FFFFFF'
+	//             }} activeClassName="nav-item active" activeStyle={{
+	//                 color: '#F59B45'
+	//             }}>Solo Flotilia</IndexLink>
+	//             <span>      </span>
+	//             <IndexLink to="/fleet" className="nav-item" style={{
+	//                 fontSize: '115%',
+	//                 color: '#FFFFFF'
+	//             }} activeClassName="nav-item active" activeStyle={{
+	//                 color: '#F59B45'
+	//             }}>Main Fleet</IndexLink>
+	//             <span>      </span>
+	//             <IndexLink to="/profile" className="nav-item" style={{
+	//                 fontSize: '115%',
+	//                 color: '#FFFFFF'
+	//             }} activeClassName="nav-item active" activeStyle={{
+	//                 color: '#F59B45'
+	//             }}>Profile</IndexLink>
+	//         </nav>
+	//     );
+	// };
+
 	var Nav = function Nav() {
 	    return React.createElement(
 	        'nav',
@@ -24956,7 +25001,7 @@
 	        ),
 	        React.createElement(
 	            IndexLink,
-	            { to: '/profile', className: 'nav-item', style: {
+	            { to: '/profile', className: 'nav-item float-xs-right', style: {
 	                    fontSize: '115%',
 	                    color: '#FFFFFF'
 	                }, activeClassName: 'nav-item active', activeStyle: {
