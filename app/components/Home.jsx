0fefb175592e235c997b583d9dc719750a66a3af
shortcircuit1024 <shortcircuit1024@gmail.com>
2017-01-04 21:32:52 -0600

    Updates to scheduler

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Home = (props) => {
     return (
         <div className="row container-fluid" id="homeLanding">
-          <div className="col-sm-8 offset-sm-2">
+          <div className="col-sm-8 offset-sm-2" >
             <h3 className="landingHeader" style={{ fontFamily: 'Electrolize', textAlign: 'center', marginTop: '25%', color: 'white'}}>Armitage Mobile Militia Offensive</h3>
             <h1 className="landingHeader" style={{ fontFamily: 'Electrolize', textAlign: 'center', marginBottom: '0%', color: '#F59B45'}}><b> A . M . M . O . </b></h1>
             <p className="landingText lead" style={{ fontFamily: 'Offside', textAlign: 'center'}}><small>ex oblivione, unitas</small></p>
--- app/components/Scheduler.jsx
+++ app/components/Scheduler.jsx
@@ -2,12 +2,16 @@ var React = require('react');
 var Clock = require('react-clock');
 
 var Scheduler = React.createClass({
-
     render: function() {
+      var d = new Date();
+      var n = d.getTimezoneOffset();
+      var s = d.toTimeString();
         return (
             <div style={{padding: '53px'}}>
                 <h3>Welcome to the AMMO Scheduler</h3>
                 <Clock />
+                <p>{s}</p>
+                <p>Offset {(n/60) * -1}</p>
             </div>
         );
     }
